c558f6f68bb37bdcfcd970751bbdf48a524163fc
wangqin <wangqin@zhongan.com>
2018-05-16 19:00:49 +0800

    周一

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 module.exports = {
-    entry: path.resolve(__dirname, 'lib/index.js'),
+    entry: path.resolve(__dirname, 'src/index.js'),
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'build'),
         filename: 'index.bundle.js'
     },
     module: {
