d290a5bdba4029bf97773f443f8ab268ca1a694f
wangqin <wangqin@zhongan.com>
2018-09-14 19:01:32 +0800

    周一

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,3 @@
 module.exports = {
-  'extends': 'airbnb',
-  'plugins': [
-    'react',
-    'jsx-a11y',
-    'import',
-  ],
-  'globals': {
-    'document': true,
-    'window': true,
-    'global': true,
-    'process': true,
-    '__dirname': true,
-  },
-  "rules": {
-    "import/no-named-as-default": 0,
-    "import/no-named-as-default-member": 0,
-    "react/jsx-filename-extension": 0
-  }
-}
+    "extends": "standard"
+};
